3c7d11c1e67d8149035f83c1093dd023d1336a50
Yoshitha Peddireddy <yoshee257@gmail.com>
2024-03-14 09:11:27 +0530

    modified_astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Yoshi1705.github.io',
-  base: '/bootcamp-dev-replica-Yoshi1705',
   integrations: [tailwind()]
 });
